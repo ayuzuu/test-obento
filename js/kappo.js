56b89bd0f8801a160b035f0dcebad8f56911c764
ayuzuu <ayuzuuuu@gmail.com>
2019-11-19 01:57:44 +0900

    デザイン調整 og-img追加

--- js/kappo.js
+++ js/kappo.js
@@ -5,3 +5,21 @@
 //   centerPadding:'10%', //両サイドの見えている部分のサイズ
 //   autoplay:true, //自動再生
 // });
+$(function () {
+    $(window).on('load resize', function () {
+        var w = $(window).width()
+        if (800 <= w) {
+            // PC
+            $("#nav1 ul").removeClass("slick-slider")
+            $("#nav1 ul").removeClass("slick-initialized")
+
+            $("#nav1 ul").addClass("lunch-list")
+        } else {
+            // SP
+            $("#nav1 ul").addClass("slick-slider")
+            $("#nav1 ul").addClass("slick-initialized")
+
+            $("#nav1 ul").removeClass("lunch-list")
+        }
+    });
+});
