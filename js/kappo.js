a0da63b2ef643aaf90a33dea612919b1705a3105
ayuzuu <ayuzuuuu@gmail.com>
2019-11-21 01:33:22 +0900

    デザイン調整

--- js/kappo.js
+++ js/kappo.js
@@ -1,25 +1,15 @@
-// $('.center').slick({
-//   infinite: true, //スライドのループ有効化
-//   dots:true, //ドットのナビゲーションを表示
-//   centerMode: true, //要素を中央寄せ
-//   centerPadding:'10%', //両サイドの見えている部分のサイズ
-//   autoplay:true, //自動再生
-// });
+// お弁当一覧のクラス出し分け
 $(function () {
     $(window).on('load resize', function () {
         var w = $(window).width()
         if (800 <= w) {
             // PC
-            $("#nav1 ul").removeClass("slick-slider")
-            $("#nav1 ul").removeClass("slick-initialized")
-
-            $("#nav1 ul").addClass("lunch-list")
+            $("#sp-lunch-list").hide()
+            $("#pc-lunch-list").show()
         } else {
             // SP
-            $("#nav1 ul").addClass("slick-slider")
-            $("#nav1 ul").addClass("slick-initialized")
-
-            $("#nav1 ul").removeClass("lunch-list")
+            $("#sp-lunch-list").show()
+            $("#pc-lunch-list").hide()
         }
     });
 });
