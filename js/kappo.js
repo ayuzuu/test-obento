a31cc3628be1b7e30830233504037a8220cf5b87
ayuzuu <ayuzuuuu@gmail.com>
2019-11-25 00:18:33 +0900

    pc mv画像追加、調整

--- js/kappo.js
+++ js/kappo.js
@@ -1,15 +1,56 @@
 // お弁当一覧のクラス出し分け
 $(function () {
+    // スライドショーの設定
+    var slideshowOptionsPc = {
+        src: [
+            { url: "/img/bg-mv01-pc.jpg" },
+            { url: "/img/bg-mv02-pc.jpg" },
+            { url: "/img/bg-mv03-pc.jpg" },
+        ],
+        duration: 6,
+        fade: 2,
+        scaling: 1.2,
+        rotating: false,
+        overlay: "",
+    }
+    var slideshowOptionsSp = {
+        src: [
+            { url: "/img/bg-mv01.png" },
+            { url: "/img/bg-mv02.png" },
+            { url: "/img/bg-mv03.png" },
+        ],
+        duration: 6,
+        fade: 2,
+        scaling: 1.2,
+        rotating: false,
+        overlay: "",
+    }
+
+
+    // スライドショー初期化
+    $.sublime_slideshow(slideshowOptionsSp);
+    $("ul.sm-slider").appendTo("#container");
+
+    // サイズ変更時イベント
     $(window).on('load resize', function () {
         var w = $(window).width()
         if (800 <= w) {
             // PC
+            // メニューの切り替え
             $("#sp-lunch-list").hide()
             $("#pc-lunch-list").show()
+
+            // スライドショーの切り替え
+            $.sublime_slideshow.changeOptions(slideshowOptionsPc)
+
         } else {
             // SP
+            // メニューの切り替え
             $("#sp-lunch-list").show()
             $("#pc-lunch-list").hide()
+
+            // スライドショーの切り替え
+            $.sublime_slideshow.changeOptions(slideshowOptionsSp)
         }
     });
 });
